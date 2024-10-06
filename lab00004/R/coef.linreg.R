7fe87b8028ed44f8a1812744766efef226df7b3b
hanxi898 <hanxi898@student.liu.se>
2024-10-06 13:12:07 +0200

    Update coef.linreg.R

--- lab00004/R/coef.linreg.R
+++ lab00004/R/coef.linreg.R
@@ -4,6 +4,6 @@
 #' @param ... Additional arguments to be passed to or from methods.
 #' @return A named vector of coefficients
 #' @export
-coef.linreg <- function(X,...) {
-  return(setNames(X$coefficients, paste0("Coefficient ", seq_along(X$coefficients))))
+coef.linreg <- function(object,...) {
+  return(setNames(object$coefficients, paste0("Coefficient ", seq_along(object$coefficients))))
 }
