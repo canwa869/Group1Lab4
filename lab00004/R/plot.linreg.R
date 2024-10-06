d65ed6225dc84e92d34efa8d16ec6d10fac10919
hanxi898 <hanxi898@student.liu.se>
2024-10-06 13:13:50 +0200

    Update plot.linreg.R

--- lab00004/R/plot.linreg.R
+++ lab00004/R/plot.linreg.R
@@ -12,35 +12,33 @@
 #' @import ggplot2
 #' @importFrom stats fitted residuals rstandard
 #' @export
-plot.linreg <- function(linreg_mod){
-  fitted_values <- fitted(linreg_mod)
-  residuals <- residuals(linreg_mod)
-  std_residuals <- rstandard(linreg_mod)
+plot.linreg <- function(object){
+  fitted_values <- fitted(object)
+  residuals <- residuals(object)
+  std_residuals <- rstandard(object)
   plot_data <- data.frame(fitted_values, residuals, std_residuals)
   plot_data$index <- 1:nrow(plot_data)
   p1 <- ggplot(plot_data, aes(x = fitted_values, y = residuals)) +
-    geom_point(shape = 1, 
-               size = 2, 
+    geom_point(shape = 1,
+               size = 2,
                color = "black") +
     geom_smooth(se = FALSE, color = "red") +
-    geom_text(aes(label = ifelse(abs(residuals) > 1.1, index, "")), 
-              hjust = -0.2, vjust = -0.5) + 
+    geom_text(aes(label = ifelse(abs(residuals) > 1.1, index, "")),
+              hjust = -0.2, vjust = -0.5) +
     labs(x = "Fitted values\nlm(Petal.Length ~ Species)",
          y = "Residuals", title = "Residuals vs Fitted") +
     theme_minimal()
-  
+
   print(p1)
   p2 <- ggplot(plot_data, aes(x = fitted_values, y = sqrt(abs(std_residuals)))) +
     geom_point(shape = 1,
-               size = 2, 
+               size = 2,
                color = "black") +
     geom_smooth(se = FALSE, color = "red") +
-    geom_text(aes(label = ifelse(sqrt(abs(std_residuals)) > 1.6, index, "")), 
+    geom_text(aes(label = ifelse(sqrt(abs(std_residuals)) > 1.6, index, "")),
               hjust = -0.2, vjust = -0.5) +
     labs(x = "Fitted values\nlm(Petal.Length ~ Species)",
          y = expression(sqrt("Standardized residuals")), title = "Scale-Location") +
     theme_minimal()
   print(p2)
 }
-linreg_mod <- lm(Petal.Length ~ Species, data = iris)
-plot.linreg(linreg_mod)
