53b7a9d1637e7212f4584780b027062b18a3270f
hanxi898 <hanxi898@student.liu.se>
2024-10-06 13:45:19 +0200

    Update resid.linreg.R

--- lab00004/R/resid.linreg.R
+++ lab00004/R/resid.linreg.R
@@ -4,6 +4,9 @@
 #' @param ... Additional arguments to be passed to or from methods.
 #' @return A vector of residuals
 #' @export
+resid <- function(object,...){
+  UseMethod("resid")
+} 
 resid.linreg <- function(object,...) {
   return(object$residuals)
 }
