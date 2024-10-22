954be75b3201cfe733238329208ac690e21d6f48
hanxi898 <hanxi898@student.liu.se>
2024-10-22 19:04:38 +0200

    delete .R files

--- R/pred.linreg.R
+++
@@ -1,9 +0,0 @@
-#' Predicted values method
-#'
-#' @param object An object
-#' @return A vector of predicted values
-#' @export
-pred.linreg <-
-  function(object) {
-    return(object$fitted_values)
-  }
