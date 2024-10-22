5bca07e87f1a7b43b7da349531117503be2b9f93
hanxi898 <hanxi898@student.liu.se>
2024-10-22 17:30:32 +0200

    Delete lab00004 directory

--- lab00004/R/coef.linreg.R
+++
@@ -1,9 +0,0 @@
-#' Coefficients method
-#'
-#' @param X An object
-#' @param ... Additional arguments to be passed to or from methods.
-#' @return A named vector of coefficients
-#' @export
-coef.linreg <- function(object,...) {
-  return(setNames(object$coefficients, paste0("Coefficient ", seq_along(object$coefficients))))
-}
